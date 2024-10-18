06bb15ce7c3ab36e5dc3bcf2dcbca77d19f152a1
Jewell <jewell.braden@nordstrom.com>
2024-10-18 16:00:02 -0700

    Initial Commit

---
+++ Corkboard 2.0/Corkboard_2_0App.swift
@@ -0,0 +1,17 @@
+//
+//  Corkboard_2_0App.swift
+//  Corkboard 2.0
+//
+//  Created by dxek on 10/18/24.
+//
+
+import SwiftUI
+
+@main
+struct Corkboard_2_0App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
